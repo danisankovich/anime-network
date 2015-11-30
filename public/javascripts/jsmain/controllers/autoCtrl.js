d4e188dffcba8fa8433fa91f929df5d89bbb6ad7
mikesankovich <misankovich@gmail.com>
2015-11-29 21:27:01 -0800

    autocomplete accurately leads to pages

--- public/javascripts/jsmain/controllers/autoCtrl.js
+++ public/javascripts/jsmain/controllers/autoCtrl.js
@@ -1,22 +1,35 @@
 app.controller('autoCtrl', function($scope, $state, $http){
-  // $(document).ready(function() {
-  //   console.log('yes');
-  //   var availableTags = [];
-  //   $http.get('http://localhost:4000/anime').success(function(anime) {
-  //     anime.forEach(function(e) {
-  //       if(e.title !== undefined) {
-  //         availableTags.push(e.title);
-  //       }
-  //     });
-  //   });
-  //   $( "#tags" ).autocomplete({
-  //     minLength:2,
-  //     source: availableTags
-  //   });
+  $(document).ready(function() {
+    console.log('yes');
+    var availableTags = [];
+    $http.get('http://localhost:4000/anime').success(function(anime) {
+      anime.forEach(function(e) {
+        if(e.title !== undefined) {
+          availableTags.push(e.title);
+        }
+      });
+    });
+    $( "#tags" ).autocomplete({
+      minLength:2,
+      source: availableTags,
+      select: function(event, ui) {
+        $(this).val(ui.item.value);
+        console.log("hi", $(this).val());
+        $scope.anime = $(this).val().toLowerCase().split(" ").join("-");
+      }
+    });
 
-  $scope.searchAnime = function(anime) {
-    anime = this.anime.toLowerCase().split(" ").join("-");
+  $scope.searchAnime = function() {
+    anime = $scope.anime;
+    // console.log(this);
     $state.go('anime', {animename: anime});
   };
-  // });
+  $http.get('http://localhost:4000/user').success(function(user) {
+    if(user) {
+      console.log("user", user);
+      $scope.currentUser = user.username;
+    }
+  });
+  });
+
 });
