2d1402ef18e112347302c426e0237be567b88f9e
michael sankovich <misankovich@gmail.com>
2016-01-31 18:20:11 -0500

    deploy

--- public/javascripts/jsmain/controllers/forumCtrl.js
+++ public/javascripts/jsmain/controllers/forumCtrl.js
@@ -1,6 +1,6 @@
 app.controller('forumCtrl', function($scope, $state, $http, animeService, userService){
-  $scope.whichUrl = 'http://localhost:4000';
-  // $scope.whichUrl = 'http://animenetwork.herokuapp.com';
+  // $scope.whichUrl = 'http://localhost:4000';
+  $scope.whichUrl = 'http://animenetwork.herokuapp.com';
   userService.getCurrentUser().success(function(data) {
     $scope.user = data;
   });
